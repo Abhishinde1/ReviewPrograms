473922374b387045f626a36ff534caa3d56c6d9c
AbhiShinde1 <abhishinde572@gmail.com>
2022-08-02 08:45:52 +0530

    Prime number

---
+++ primecheck.sh
@@ -0,0 +1,18 @@
+#!/bin/bash 
+
+echo -e "Enter number : "
+read n
+for((i=2; i<=$n/2; i++))
+do
+        ans=$(( n%i ))
+        if [ $ans -eq 0 ]
+        then
+        echo "it is not a prime"
+        exit
+        fi
+done
+echo "it is a prime number"
+
+
+
+
